34a9bf14f0677001d7321299b938e96a0c6e63b2
Jack-In <sunduo3195@qq.com>
2018-03-07 17:04:34 +0800

    修改package.json配置文件,组件写法！

---
+++ index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div ref="qr" class="qrBox">
+    <img :src="src" alt="demo">
+  </div>
+</template>
+
+<script>
+import QRCode from 'qrcode'
+export default {
+  data() {
+    return {
+      src: '',
+      textBase: 'demo',
+      configBase: {
+        // 容错等级
+        errorCorrectionLevel: 'H',
+        // 图片类型
+        type: 'image/png',
+        rendererOpts: {
+          quality: 0.3
+        },
+        // 边框与二维码之间的间距
+        margin: 2,
+        // 缩放倍数
+        scale: 4,
+        width: 500,
+        maskPattern: 1,
+        color: {
+          dark: '#000000',
+          light: '#ffffff'
+        },
+        style: {
+          width: '128px',
+          border: '1px solid #ccc'
+        }
+      }
+    }
+  },
+  props: ['config', 'text'],
+  mounted() {
+    this.makeQRCode(this.config, this.text)
+  },
+  methods: {
+    makeQRCode(config, text) {
+      QRCode.toDataURL(
+        text || this.textBase,
+        config || this.configBase,
+        (err, url) => {
+          console.log(err)
+          this.src = url
+        })
+      if (!config) {
+        for (const k in this.configBase.style) {
+          this.$refs.qr.style[k] = this.configBase.style[k]
+        }
+      } else {
+        for (const k in config.style) {
+          this.$refs.qr.style[k] = this.config.style[k]
+        }
+      }
+    }
+  }
+}
+</script>
+<style>
+.qrBox img {
+  vertical-align: top;
+  width: 128px;
+}
+</style>
